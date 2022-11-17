a928c70f611a9ca840da0acf4e41b16aa2eecffc
KainanSilvaaSiqueiraa0 <115712148+KainanSilvaaSiqueiraa0@users.noreply.github.com>
2022-11-17 11:06:39 -0300

    Add files via upload

---
+++ desafio 5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace desafio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("leia o valor de a");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("leia o valor de b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("leia o valor de c");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ desafio 8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace desafio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira um numero");
+            double numero = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt(numero));
+
+
+        }
+    }
+}
