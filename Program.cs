9ed0fe36c0fbbc46c2a5b15ed422f206e1c6d42d
KainanSilvaaSiqueiraa0 <115712148+KainanSilvaaSiqueiraa0@users.noreply.github.com>
2022-11-18 15:43:09 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,12 @@
 ﻿using System;
 
-namespace desafio_1
+namespace juridico
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("primeiro desafio");
-
-            try
-            {
-
-                Console.WriteLine("imforme um valor");
-
-                int x = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("numero");
-            }
-            catch (FormatException)
-            {
-
-                Console.WriteLine("texto");
-            }
-
-
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
